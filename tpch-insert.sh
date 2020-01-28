b1f45d8053fcf75400bd213739fe5f66cf09ae99
chertus <chertus@gmail.com>
2020-01-28 23:23:36 +0300

    tables in upper case for TPC-H

--- tpch-insert.sh
+++ tpch-insert.sh
@@ -3,11 +3,11 @@ CLIENT="$CLICKHOUSE_CLIENT --format_csv_delimiter=|"
 
 DIR=$1
 
-$CLIENT -q 'insert into tpch.part format CSV'       < $DIR/part.tbl
-$CLIENT -q 'insert into tpch.supplier format CSV'   < $DIR/supplier.tbl
-$CLIENT -q 'insert into tpch.partsupp format CSV'   < $DIR/partsupp.tbl
-$CLIENT -q 'insert into tpch.customer format CSV'   < $DIR/customer.tbl
-$CLIENT -q 'insert into tpch.orders format CSV'     < $DIR/orders.tbl
-$CLIENT -q 'insert into tpch.lineitem format CSV'   < $DIR/lineitem.tbl
-$CLIENT -q 'insert into tpch.nation format CSV'     < $DIR/nation.tbl
-$CLIENT -q 'insert into tpch.region format CSV'     < $DIR/region.tbl
+$CLIENT -q 'insert into tpch.PART format CSV'       < $DIR/part.tbl
+$CLIENT -q 'insert into tpch.SUPPLIER format CSV'   < $DIR/supplier.tbl
+$CLIENT -q 'insert into tpch.PARTSUPP format CSV'   < $DIR/partsupp.tbl
+$CLIENT -q 'insert into tpch.CUSTOMER format CSV'   < $DIR/customer.tbl
+$CLIENT -q 'insert into tpch.ORDERS format CSV'     < $DIR/orders.tbl
+$CLIENT -q 'insert into tpch.LINEITEM format CSV'   < $DIR/lineitem.tbl
+$CLIENT -q 'insert into tpch.NATION format CSV'     < $DIR/nation.tbl
+$CLIENT -q 'insert into tpch.REGION format CSV'     < $DIR/region.tbl
